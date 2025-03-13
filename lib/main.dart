3c4cdf59f323536d1888d9c5ee49f446c8c301a3
Tsukamoto Yuto <yuto.tsukamoto@ncdc.co.jp>
2025-03-13 10:47:06 +0900

    feat: 多言語化対応の実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,59 +11,96 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+
+      // locale: const Locale('zh'), // ここで設定されたロケールに基づいて表示が変わります
+      // localizationsDelegates: const [
+      //   GlobalMaterialLocalizations.delegate,
+      //   GlobalWidgetsLocalizations.delegate,
+      //   GlobalCupertinoLocalizations.delegate,
+      // ],
+      // supportedLocales: const [
+      //   Locale('en'),
+      //   Locale('zh'),
+      //   Locale('ja'),
+      // ],
+
+      // localizationsDelegates: const [
+      //   AppLocalizations.delegate,
+      //   GlobalMaterialLocalizations.delegate,
+      //   GlobalWidgetsLocalizations.delegate,
+      //   GlobalCupertinoLocalizations.delegate,
+      // ],
+      // supportedLocales: const [
+      //   Locale('en'), // English
+      //   Locale('ja'), // Spanish
+      //   Locale('zh'), // Chinese
+      // ],
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final languageSetting = _getCurrentLanguageSetting(context);
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text("multi language sample"),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+            Text('システム言語設定: $languageSetting'),
+            const SizedBox(height: 40),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              "text: ${AppLocalizations.of(context)!.text}",
+              style: Theme.of(context).textTheme.headlineSmall,
             ),
+            const SizedBox(height: 40),
+
+            CalendarDatePicker(
+              initialDate: DateTime.now(),
+              firstDate: DateTime(1900),
+              lastDate: DateTime(2100),
+              onDateChanged: (value) {},
+            ),
+
+            // Localizations.override(
+            //   context: context,
+            //   locale: const Locale('es'),
+            //   child: Builder(
+            //     builder: (context) {
+            //       return CalendarDatePicker(
+            //         initialDate: DateTime.now(),
+            //         firstDate: DateTime(1900),
+            //         lastDate: DateTime(2100),
+            //         onDateChanged: (value) {},
+            //       );
+            //     },
+            //   ),
+            // ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
     );
   }
+}
+
+/// 現在の言語設定を取得する
+String _getCurrentLanguageSetting(BuildContext context) {
+  Locale locale = Localizations.localeOf(context);
+  String languageCode = locale.languageCode;
+  return languageCode;
 }
